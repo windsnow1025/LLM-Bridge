f9a140c03aa37a6bea65f92e92fd295518fd15a1
windsnow1025 <windsnow1025@gmail.com>
2025-02-17 08:42:20 -0800

    build: long description

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='LLM-Bridge',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'fastapi',
@@ -24,6 +27,8 @@ setup(
     author='windsnow1025',
     author_email='windsnow125@gmail.com',
     description='A Bridge for LLMs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     python_requires='>=3.12',
 )
