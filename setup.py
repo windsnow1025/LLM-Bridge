dec5218dd8db161d18ad73ac5e3c04798feebd4a
windsnow1025 <windsnow1025@gmail.com>
2025-03-03 10:44:29 -0800

    build: freeze versions

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='0.3.6',
+    version='0.3.7',
     packages=find_packages(),
     install_requires=[
-        'fastapi',
-        'httpx',
-        'openai',
-        'google-genai',
-        'anthropic',
-        'tiktoken',
-        'pillow',
-        'PyMuPDF',
-        'python-docx',
-        'openpyxl',
-        'python-pptx',
-        'pytest',
-        'pytest-asyncio',
-        'python-dotenv',
-        'protobuf'
+        'fastapi==0.115.9',
+        'httpx==0.28.1',
+        'openai==1.63.2',
+        'google-genai==1.2.0',
+        'anthropic==0.47.1',
+        'PyMuPDF==1.25.3',
+        'python-docx==1.1.2',
+        'openpyxl==3.1.5',
+        'python-pptx==1.0.2',
+    ],
+    tests_require=[
+        'pytest==8.3.4',
+        'pytest-asyncio==0.25.3'
+        'python-dotenv==1.0.1',
+        'protobuf==5.29.3'
     ],
     author='windsnow1025',
     author_email='windsnow125@gmail.com',
