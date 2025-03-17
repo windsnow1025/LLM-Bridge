33b6efe4a090eb02dd867dd2d3b0762eb42cd03e
windsnow1025 <windsnow1025@gmail.com>
2025-03-17 09:00:12 -0700

    build: 1.0.6 alpha

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='1.0.5',
+    version='1.0.6-alpha',
     packages=find_packages(),
     install_requires=[
         'fastapi',
