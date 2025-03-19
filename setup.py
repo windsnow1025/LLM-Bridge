14dbe30ce020699fd77377c0a32070a61c066758
windsnow1025 <windsnow1025@gmail.com>
2025-03-19 07:24:20 -0700

    build: remove google genai version requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='1.0.6-alpha',
+    version='1.0.7',
     packages=find_packages(),
     install_requires=[
         'fastapi',
