97eea8ae187e983a34a5044a99d83c10e40f12f5
windsnow1025 <windsnow1025@gmail.com>
2025-03-25 21:47:33 -0700

    build: include json

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='1.1.2-alpha1',
+    version='1.1.2-alpha2',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'fastapi',
         'httpx',
