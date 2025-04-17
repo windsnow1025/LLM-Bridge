039b3c3b0f3e00dff57c27b0336a5e3a24f3f8db
windsnow1025 <windsnow1025@gmail.com>
2025-04-17 10:01:28 -0700

    build: upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='1.3.0-alpha1',
+    version='1.3.0-alpha2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
