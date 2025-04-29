3bcb2b08e02fe077fa8b58e4c993f76155c01e43
windsnow1025 <windsnow1025@gmail.com>
2025-04-29 04:58:26 -0700

    fix: gemini grounding

--- llm_bridge/client/implementations/gemini/gemini_response_handler.py
+++ llm_bridge/client/implementations/gemini/gemini_response_handler.py
@@ -41,24 +41,26 @@ class GeminiResponseHandler:
                     image_base64 = base64.b64encode(part.inline_data.data).decode('utf-8')
 
         # Grounding Sources
-        if grounding_metadata := response.candidates[0].grounding_metadata:
-            if search_entry_point := grounding_metadata.search_entry_point:
-                display = search_entry_point.rendered_content
-            if grounding_metadata.grounding_chunks:
-                text += "\n\n# Grounding Sources:\n"
-                for i, chunk in enumerate(grounding_metadata.grounding_chunks, start=1):
-                    if chunk.web:
-                        text += f"{i}. [{chunk.web.title}]({chunk.web.uri})\n"
+        if candidates := response.candidates:
+            if grounding_metadata := candidates[0].grounding_metadata:
+                if search_entry_point := grounding_metadata.search_entry_point:
+                    display = search_entry_point.rendered_content
+                if grounding_metadata.grounding_chunks:
+                    text += "\n\n# Grounding Sources:\n"
+                    for i, chunk in enumerate(grounding_metadata.grounding_chunks, start=1):
+                        if chunk.web:
+                            text += f"{i}. [{chunk.web.title}]({chunk.web.uri})\n"
 
         return ChatResponse(text=text, image=image_base64, display=display, citations=citations)
 
 
 def extract_citations(response: types.GenerateContentResponse) -> list[Citation]:
     citations = []
-    if grounding_metadata := response.candidates[0].grounding_metadata:
-        if grounding_supports := grounding_metadata.grounding_supports:
-            for grounding_support in grounding_supports:
-                citation_indices = [index + 1 for index in grounding_support.grounding_chunk_indices]
-                citation_text = grounding_support.segment.text
-                citations.append(Citation(text=citation_text, indices=citation_indices))
+    if candidates := response.candidates:
+        if grounding_metadata := candidates[0].grounding_metadata:
+            if grounding_supports := grounding_metadata.grounding_supports:
+                for grounding_support in grounding_supports:
+                    citation_indices = [index + 1 for index in grounding_support.grounding_chunk_indices]
+                    citation_text = grounding_support.segment.text
+                    citations.append(Citation(text=citation_text, indices=citation_indices))
     return citations
