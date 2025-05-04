a404af50eae602dc18ad0bdf80cf1b5437a22deb
windsnow1025 <windsnow1025@gmail.com>
2025-05-04 04:42:06 -0700

    chore: improve media processor

--- llm_bridge/logic/chat_generate/media_processor.py
+++ llm_bridge/logic/chat_generate/media_processor.py
@@ -9,12 +9,11 @@ async def get_raw_content_from_url(req_url: str) -> tuple[bytes, str]:
 
 
 async def get_encoded_content_from_url(req_url: str) -> tuple[str, str]:
-    img_data, media_type = await fetch_file_data(req_url)
+    img_data, media_type = await get_raw_content_from_url(req_url)
     base64_image = base64.b64encode(img_data).decode('utf-8')
     return base64_image, media_type
 
 
 async def get_openai_image_content_from_url(req_img_url: str) -> str:
-    img_data, media_type = await fetch_file_data(req_img_url)
-    base64_image = base64.b64encode(img_data).decode('utf-8')
+    base64_image, media_type = await get_encoded_content_from_url(req_img_url)
     return f"data:{media_type};base64,{base64_image}"
