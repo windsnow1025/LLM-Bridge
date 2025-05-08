944a5911cc96a5e37c9457db1f76413a7cbf735b
windsnow1025 <windsnow1025@gmail.com>
2025-05-08 08:07:41 -0700

    build: upgrade to stable version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='LLM-Bridge',
-    version='1.6.1-beta.1',
+    version='1.6.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
