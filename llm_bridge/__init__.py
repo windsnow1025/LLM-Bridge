9e44e1a3794ffb50713c552e808084c5b8d96dac
windsnow1025 <windsnow1025@gmail.com>
2025-05-25 00:03:06 -0700

    feat: export find model prices

--- llm_bridge/__init__.py
+++ llm_bridge/__init__.py
@@ -1,7 +1,7 @@
 from .logic.chat_generate.chat_client_factory import create_chat_client
 from .logic.chat_generate.chat_message_converter import *
 from .logic.message_preprocess.message_preprocessor import preprocess_messages
-from .logic.model_prices import ModelPrice, get_model_prices, calculate_chat_cost
+from .logic.model_prices import ModelPrice, get_model_prices, find_model_prices, calculate_chat_cost
 from .type.chat_response import Citation, ChatResponse
 from .type.message import Role, Message, Content, ContentType
 from .type.serializer import serialize
