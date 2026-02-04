1074bd0c18e472ee166d4163c900ef05e9e0a649
windsnow1025 <windsnow1025@gmail.com>
2026-02-03 23:33:23 -0500

    feat: export file type

--- llm_bridge/__init__.py
+++ llm_bridge/__init__.py
@@ -2,6 +2,6 @@ from .logic.chat_generate.chat_client_factory import create_chat_client
 from .logic.chat_generate.chat_message_converter import *
 from .logic.message_preprocess.message_preprocessor import preprocess_messages
 from .logic.model_prices import ModelPrice, get_model_prices, find_model_prices, calculate_chat_cost
-from .type.chat_response import ChatResponse
+from .type.chat_response import ChatResponse, File
 from .type.message import Role, Message, Content, ContentType
 from .type.serializer import serialize
